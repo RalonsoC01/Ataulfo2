5befa830521b4f3fa1b3b041b9996104e1febec7
RalonsoC01 <72815754+RalonsoC01@users.noreply.github.com>
2021-11-28 03:09:07 +0100

    Todo completado

--- app/src/main/java/com/example/ataulfo/FP.java
+++ app/src/main/java/com/example/ataulfo/FP.java
@@ -2,7 +2,12 @@ package com.example.ataulfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FP extends AppCompatActivity {
 
@@ -10,5 +15,50 @@ public class FP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fp);
+    }
+
+    public void cocina (View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "Accediendo a la web";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intentNavegador = new Intent(Intent.ACTION_VIEW, Uri.parse("http://iesataulfoargenta.es/wp/category/ensenanzas/formacion-profesional/cocina-y-restauracion/"));
+        startActivity(intentNavegador);
+    }
+    public void smr (View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "Accediendo a la web";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intentNavegador = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.iesataulfoargenta.es/ciclosinformatica/sistemas-microinformaticos-y-redes/sistemas-microinformaticos-y-redes/"));
+        startActivity(intentNavegador);
+    }
+    public void dam (View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "Accediendo a la web";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intentNavegador = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.iesataulfoargenta.es/ciclosinformatica/desarrollo-de-aplicaciones-multiplataforma/desarrollo-de-aplicaciones-multiplataforma/"));
+        startActivity(intentNavegador);
+    }
+    public void daw (View view)
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "Accediendo a la web";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intentNavegador = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.iesataulfoargenta.es/ciclosinformatica/desarrollo-de-aplicaciones-web/desarrollo-de-aplicaciones-web/"));
+        startActivity(intentNavegador);
     }
 }
